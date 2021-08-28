40b86e56f63538154e86d5b9fa4cb0bfb634803d
Emre <eemmrree0@outlook.com>
2021-08-28 18:50:59 +0300

    BaseEntity de düzenleme yapıldı.

--- Entities/Concrete/BaseEntities/BaseEntity.cs
+++ Entities/Concrete/BaseEntities/BaseEntity.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities.Abstract;
 
 namespace Entities.Concrete.BaseEntities
 {
-    public class BaseEntity
+    public class BaseEntity : IEntity
     {
+        public int Id { get; set; }
     }
 }
