c9126b015b8657473e87b65aa3aa01dbd4b89005
Emre <eemmrree0@outlook.com>
2021-08-28 18:48:22 +0300

    DataAccess de UserMap oluşturuldu.

---
+++ DataAccess/Concrete/EntityFramework/Mapping/UserMap.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Concrete.EntityFramework.Mapping
+{
+    public class UserMap: IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users", @"dbo");
+            builder.HasKey(x=>x.Id);
+            builder.Property(x => x.UserName)
+                .HasColumnName("UserName")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.FirstName)
+                .HasColumnName("FirstName")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.LastName)
+                .HasColumnName("LastName")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.Password)
+                .HasColumnName("Password")
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(x => x.Gender)
+                .HasColumnName("Gender")
+                .IsRequired();
+
+            builder.Property(x => x.DateOfBirth)
+                .HasColumnName("DateOfBirth")
+                .IsRequired();
+            builder.Property(x => x.CreatedDate).HasDefaultValue(DateTime.Now);
+
+            builder.HasData(new User
+            {
+                Id = 1,
+                FirstName = "Emre",
+                LastName = "Bayrak",
+                Password = "12345",
+                Gender = true,
+                DateOfBirth = Convert.ToDateTime("12-30-1997"),
+                CreatedDate = DateTime.Now,
+                Address = "CORUM",
+                CreatedUserId = 1,
+                Email = "emre@gmail.com",
+                UserName = "emrebayrak"
+            });
+        }
+    }
+}
