fe85a977ac0d1a46db816106b7cbf6cca360bc84
Emre <eemmrree0@outlook.com>
2021-08-29 18:04:38 +0300

    Revert "UserDto da düzenlemeler yapıldı."
    This reverts commit b9bdd8f33b01e4c9a48abe1f32695ee7e3b5c3f7.

--- Entities/Dtos/UserDtos/UserDto.cs
+++ Entities/Dtos/UserDtos/UserDto.cs
@@ -17,6 +17,5 @@ namespace Entities.Dtos.UserDtos
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
-        public string Password { get; set; }
     }
 }
