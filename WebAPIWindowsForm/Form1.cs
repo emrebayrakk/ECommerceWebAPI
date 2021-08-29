bcf0c350a46a1d39f04e54a9864fab5002a10153
Emre <eemmrree0@outlook.com>
2021-08-29 15:52:41 +0300

    WebAPIWindowsForm da veriler listelendi.

--- WebAPIWindowsForm/Form1.cs
+++ WebAPIWindowsForm/Form1.cs
@@ -1,20 +1,28 @@
-﻿using System;
+﻿
+using Entities.Dtos.UserDtos;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Windows.Forms;
 
 namespace WebAPIWindowsForm
 {
     public partial class Form1 : Form
     {
+        private string url = "http://localhost:28493/api/Users/GetList";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            using (HttpClient httpClient=new HttpClient())
+            {
+                var users =await httpClient.GetFromJsonAsync<List<UserDetailDto>>(new Uri(url));
+                dataGridView1.DataSource = users;
+            }
         }
     }
 }
