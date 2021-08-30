567f764c1675dcabe28b8f6f03f9b2430116c888
Emre <eemmrree0@outlook.com>
2021-08-30 15:53:22 +0300

    WebAPICoreMvc projesinde ViewModels de UserAddViewModel oluşturuldu.

---
+++ WebAPICoreMvc/ViewModels/UserAddViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPICoreMvc.ViewModels
+{
+    public class UserAddViewModel
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public int GenderID { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
